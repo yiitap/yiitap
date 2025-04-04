8f4020632b56c398850f26fb7477ca8115bdbb90
pileax <ipileax@gmail.com>
2025-04-04 11:32:54 +0800

    feat(example): 🎸 minimal example

---
+++ examples/vue/basic/minimal/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <YiiEditor ref="yiiEditor" v-bind="editorOptions" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { YiiEditor } from '@yiitap/vue'
+
+const editorOptions = computed(() => {
+  return {
+    editable: true,
+    content: '',
+    showBubbleMenu: true,
+    showFloatingMenu: true,
+    showSideMenu: true,
+    pageView: 'page',
+    extensions: [
+      'OAiBlock',
+      'OBlockquote',
+      'OCallout',
+      'OCodeBlock',
+      'OColon',
+      'OColorHighlighter',
+      'OHeading',
+      'OImage',
+      'OParagraph',
+      'OSlash',
+      'OSlashZh',
+      'OLink',
+      'OTrailingNode',
+      'OVideo',
+    ],
+  }
+})
+</script>
