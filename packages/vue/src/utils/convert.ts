ff9ff0b1f36ef016697b931dae44c68c4deaa388
pileax <ipileax@gmail.com>
2025-10-18 20:47:19 +0800

    style(vue): 💄 convert

--- packages/vue/src/utils/convert.ts
+++ packages/vue/src/utils/convert.ts
@@ -9,7 +9,8 @@ export const toJSON = (editor: Editor, html: string): Record<string, any> => {
 
 export const systemKeymap = () => {
   const isMac =
-    typeof navigator !== 'undefined' && /Mac|iPod|iPhone|iPad/.test(navigator.platform)
+    typeof navigator !== 'undefined' &&
+    /Mac|iPod|iPhone|iPad/.test(navigator.platform)
   return {
     mod: isMac ? '⌘' : 'Ctrl',
     alt: isMac ? '⌥' : 'Alt',
@@ -20,14 +21,14 @@ export const systemKeymap = () => {
 }
 
 export const convertShortcut = (shortcut: string) => {
-  const keys = shortcut?.split('+')
-    .map(k => k.trim().toLowerCase())
+  const keys = shortcut
+    ?.split('+')
+    .map((k) => k.trim().toLowerCase())
     .filter(Boolean)
-  if (!keys || !keys.length)
-    return shortcut
+  if (!keys || !keys.length) return shortcut
 
   const map = systemKeymap()
-  const converted = keys.map(k => {
+  const converted = keys.map((k) => {
     return map[k as keyof typeof map] ?? k.toUpperCase()
   })
 
