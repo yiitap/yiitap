21710a6d0dd80bbddc517f71e795fce6cbbe88c5
pileax <ipileax@gmail.com>
2026-01-19 22:17:00 +0800

    feat(vue): 🎸 collaboration props

--- packages/vue/src/components/YiiEditor.vue
+++ packages/vue/src/components/YiiEditor.vue
@@ -225,6 +225,13 @@ const props = defineProps({
     type: Object as PropType<AiOption>,
     default: () => {},
   },
+  /**
+   * Enable collaboration or not.
+   */
+  collaboration: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 // const emit = defineEmits(['transaction', 'update'])
@@ -354,6 +361,9 @@ function buildExtensions() {
           },
       paragraph: props.extensions.includes('OParagraph') ? false : {},
       trailingNode: props.extensions.includes('OTrailingNode') ? false : {},
+      undoRedo: props.collaboration
+        ? false
+        : { depth: 100, newGroupDelay: 500 }
     })
   )
 
